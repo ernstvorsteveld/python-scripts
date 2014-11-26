efe868031fcb38b657ac00cc0365989b7267af67
Ernst Vorsteveld <ernst.vorsteveld@iwelcome.com>
2014-11-26 16:08:58 +0100

    use directory

--- files/rename.py
+++ files/rename.py
@@ -4,29 +4,33 @@ import os
 import sys
 
 class FileDetails:
-	def __init__(self, file_name, serialNumber):
+	def __init__(self, directory, file_name, serialNumber):
+		self.directory = directory
 		self.file_name = file_name
 		self.serialNumber = serialNumber		
 
-	def getNewFileName(self, printIt):
+	def rename(self, execute):
 		self.findFileDetails()
-		if(printIt):
-			print 'rename: ' + self.file_name + ' into: ' + self.name + self.serialNumber + '-' + self.number + self.fileParts[1]
+		if(execute):
+			print 'file_name: ' + self.getOldName()
+			print 'new: ' + self.getNewName()
+			os.rename(self.getOldName(), self.getNewName())
 		else:
-			print 'implement rename for: ' + self.file_name
+			print 'rename: ' + self.directory + self.file_name + ' to: ' + self.getNewName()
 
 	def findFileDetails(self):
 		self.number=''
 		self.fileParts = os.path.splitext(self.file_name)
 		fileName = self.fileParts[0]
-		while(True):
+		ready = False
+		while(not ready):
 			lastCharacter = self.getLastCharacter(fileName)
 			if(lastCharacter.isdigit()):
 				self.number += lastCharacter
 				fileName = self.removeLastCharacter(fileName)
 				self.name = fileName
 			else:
-				return
+				ready = True
 
 	def getLastCharacter(self, characters):
 		return characters[(len(characters) - 1):]
@@ -34,19 +38,44 @@ class FileDetails:
 	def removeLastCharacter(self, characters):
 		return characters[:(len(characters) - 1)]
 
+	def getNewName(self):
+		return self.directory + ('', '/')[directory.endswith('/')] + self.name + self.serialNumber + '-' + self.number + self.fileParts[1]
 
-def rename(directory, serialNumber, printIt):
-	print directory
+	def getOldName(self):
+		return self.directory + ('', '/')[directory.endswith('/')] + self.file_name		
+
+
+class Skip:
+	def __init__(self, skipFilenames):
+		self.skip = skipFilenames
+
+	def isSkip(self, file_name):
+		return file_name in self.skip
+
+
+def rename(directory, serialNumber, printIt, skip):
+	print 'rename, working on directory: ' + directory
 	for file_name in os.listdir(directory):
-		print 'working on file: ' + file_name
-		fileDetails = FileDetails(file_name, serialNumber)
-		fileDetails.getNewFileName(printIt)
+		if(skip.isSkip(file_name)):
+			continue;
+		if(os.path.isdir(file_name)):
+			print 'working on directory: ' + file_name
+			newDirectory = directory + ('/', '')[directory.endswith('/')] + file_name
+			os.chdir(directory)
+			rename(newDirectory, serialNumber, printIt, skip)
+			os.chdir('..')
+		else:
+			print 'working on file: ' + file_name
+			print 'in directory: ' + directory
+			fileDetails = FileDetails(directory, file_name, serialNumber)
+			fileDetails.rename(printIt)
 
 
 directory = sys.argv[1]
 serialNumber = sys.argv[2]
 execute = sys.argv[3] == '1'
+skip = Skip('.DS_Store')
 
-rename(directory, serialNumber, execute)
+rename(directory, serialNumber, execute, skip)
 
 
